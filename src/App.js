0c57876aa246c45242c4c9cbb1712f4e64732dbb
Xiong <xiong@ruis-mbp.attlocal.net>
2019-05-06 23:18:43 -0700

    draw disks

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import './App.css';
-
+import Towers from './components/Towers'
 
 
 function App() {
   return (
-  <div id="canves">    
-    <ul class="tower" id="tower-1"></ul>
-    <ul class="tower" id="tower-2"></ul>
-    <ul class="tower" id="tower-3"></ul>
-</div>
+  
+  <Towers />
   );
 }
 
