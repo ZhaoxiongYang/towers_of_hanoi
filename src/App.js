306324f91a8b54b7ba8b0cdf21eb18cd1970a1d5
Xiong <xiong@ruis-mbp.attlocal.net>
2019-05-06 19:32:48 -0700

    set up  towers

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './App.css';
 
+
+
 function App() {
   return (
-    <div className="App">
-
-
-
-    </div>
+  <div id="canves">    
+    <ul class="tower" id="tower-1"></ul>
+    <ul class="tower" id="tower-2"></ul>
+    <ul class="tower" id="tower-3"></ul>
+</div>
   );
 }
 
