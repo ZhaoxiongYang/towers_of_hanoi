2a264b29e6a9e1d612e3547ac028f33efb2a07e4
Xiong <xiong@ruis-mbp.attlocal.net>
2019-05-07 17:14:19 -0700

    change background color, componemece topbar

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component}from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
@@ -11,18 +11,20 @@ const styles = {
   },
 };
 
-function TopBar(props) {
-  const { classes } = props;
-  const img = <img style={{ width : 100}}src="https://certik.org/img/certik_256.png"/>;
-  return (
-    <div className={classes.root}>
-      <AppBar position="static" style={{ background: '#000000' }} >
-        <Toolbar>
-            {img}
-        </Toolbar>
-      </AppBar>
-    </div>
-  );}
-
+class TopBar extends Component {
+  render() {
+    const { classes } = this.props;
+    const img = <img style={{ width : 100}}src="https://certik.org/img/certik_256.png"/>;
+    return (
+      <div className={classes.root}>
+        <AppBar position="static" style={{ background: '#000000' }} >
+          <Toolbar>
+              {img}
+          </Toolbar>
+        </AppBar>
+      </div>
+    );
+  }
+}
 
 export default withStyles(styles)(TopBar);
